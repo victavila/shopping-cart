2ea53c6443197f3723282e6e97425ff73723cf8a
Victor Avila <victoravila1997@gmail.com>
2022-11-18 20:06:18 -0600

    Display all products on shop page

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import productsReducer from "../features/productsSlice";
 
 export const store = configureStore({
   reducer: {
+    products: productsReducer
   },
 });
 
--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,7 +1,39 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { receivedProducts } from "../features/productsSlice";
+import { getProducts } from "../utils/api";
+
 const Shop = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    getProducts().then(products => {
+      dispatch(receivedProducts(products));
+    })
+  }, [dispatch])
+
+  const products = useAppSelector(state => state.products);
+  
   return (
     <div className="shop">
-      Shop Page
+      <div>
+        <ul className="shop__items">
+          {products.map(product => (
+            <li className="shop__product" key={product.id}>
+              <div className="product">
+                <img className="product__img" src={product.image} alt={product.title}></img>
+                <div className="product__info">
+                  <h3>{product.title}</h3>
+                  <p>${product.price}</p>
+                </div>
+                <div>
+                  <button className="btn">Add to Cart</button>
+                </div>
+              </div>
+            </li>
+          )) }
+        </ul>
+      </div>
     </div>
   )
 }
