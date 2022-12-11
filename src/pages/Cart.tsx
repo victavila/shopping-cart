eb63724a3e8fcc9aca9866890468c5b516a162d5
Victor Avila <victoravila1997@gmail.com>
2022-12-11 00:29:35 -0600

    Added styling to cart page

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,67 +1,66 @@
 import { Link } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
-import { FaChevronUp, FaChevronDown, FaTrashAlt } from "react-icons/fa";
+import { FaChevronUp, FaChevronDown } from "react-icons/fa";
 import { decrementQuantity, incrementQuantity, deleteItem } from "../features/cartSlice";
 
 const Cart = () => {
   const dispatch = useAppDispatch();
 
   const { total, amount, cartItems } = useAppSelector(state => state.cart);
-  
+
   return (
     <div className="cart">
       <h2 className="cart__title">Shopping Cart</h2>
       <div className="cart__container">
+        {amount > 0 ?
         <div className="cart__items">
-          {amount > 0 ? 
-          <ul className="cart__list">
-            {cartItems.map(item => (
-              <li className="cart__listItem" key={item.id}>
-                <div className="cart__item">
-                  <div className="cart__itemImg">
-                    <img src={item.image} alt={item.title}></img>
-                  </div>
-                  <div className="cart__itemDetails">
-                    <h4 className="item__name">{item.title}</h4>
-                    <div className="item__price">${item.price}</div>
-                    <div className="item__quantity">
-                      <div>{item.quantity}</div>
-                      <div className="item__quantityBtns">
-                        <div className="increment">
-                          <FaChevronUp
-                          style={{cursor: item.quantity > 98 ? "not-allowed": "pointer"}}
-                          onClick={() => dispatch(incrementQuantity(item))} />
-                        </div>
-                        <div className="decrement">
-                          <FaChevronDown
-                          style={{cursor: item.quantity < 2 ? "not-allowed": "pointer"}}
-                          onClick={() => dispatch(decrementQuantity(item))} />
-                        </div>
+          {cartItems.map(item => (
+            <div key={item.id} className="cart__item">
+              <div className="item__image">
+                <img src={item.image} alt={item.title}></img>
+              </div>
+              <div className="item__info">
+                <div className="top__container">
+                  <h3>{item.title}</h3>
+                  <p>${(Math.round(item.price * 100) / 100).toFixed(2)}</p>
+                </div>
+                <div className="bot__container">
+                  <button className="btn__delete" onClick={() => dispatch(deleteItem(item))}>Remove Item</button>
+                  <div className="item__quantity">
+                    <div>{item.quantity}</div>
+                    <div className="quantity__btns">
+                      <div className="increment">
+                        <FaChevronUp
+                        style={{cursor: item.quantity > 98 ? "not-allowed": "pointer"}}
+                        onClick={() => dispatch(incrementQuantity(item))} />
                       </div>
-                      <div className="item__deleteBtn">
-                        <button>
-                          <FaTrashAlt onClick={() => dispatch(deleteItem(item))} />
-                        </button>
+                      <div className="decrement">
+                        <FaChevronDown
+                        style={{cursor: item.quantity < 2 ? "not-allowed": "pointer"}}
+                        onClick={() => dispatch(decrementQuantity(item))} />
                       </div>
                     </div>
                   </div>
                 </div>
-              </li>
-            ))}
-          </ul>:
-          <div>
-            <h3>CART IS EMPTY!</h3>
-            <Link to="/shop">
-              <button>Continue to shop</button>
-            </Link>
-          </div>
-          }
+              </div>
+            </div>
+          ))}
+        </div>: 
+        <div className="cart__items cart__empty">
+          <h3>CART IS EMPTY!</h3>
+          <Link to="/shop">
+            <button className="btn">Continue to shop</button>
+          </Link>
         </div>
+        }
         <div className="cart__total">
-          <div></div>
-          <div className="cart__total">Total: $ {(Math.round(total * 100) / 100).toFixed(2)}</div>
+          <h3>Cart Total</h3>
+          <div className="total__display">
+            <p>Total</p>
+            <p>${(Math.round(total * 100) / 100).toFixed(2)}</p>
+          </div>
           <div className="cart__checkout">
-            <button className="checkout__btn">Checkout</button>
+            <button className="checkout__btn btn">Proceed to Checkout</button>
           </div>
         </div>
       </div>
