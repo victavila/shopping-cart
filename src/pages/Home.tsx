7081144af8f98e85ac84383499e4beba5e64cba4
Victor Avila <victoravila1997@gmail.com>
2022-12-11 01:18:27 -0600

    Added content and styling to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+import background from "../images/home-background.jpg";
+
 const Home = () => {
+  
+  const [navigateActive, setNavigateActive] = useState(false);
+
+  const toggleNavigate = () => {
+    setNavigateActive(!navigateActive);
+  }
+  
   return (
     <div className="home">
-      Home Page
+      <img className="home__background" src={background} alt="background"></img>
+      <button className="btn__shop" onClick={() => toggleNavigate()}>Shop Now</button>
+      {navigateActive ? 
+      <Navigate to="/shop" />:
+      <></>}
     </div>
   )
 }
